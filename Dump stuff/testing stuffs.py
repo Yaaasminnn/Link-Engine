0a013b295c4101b51b4245d6786309548283b68e
MonEmperor <abr.da.king2016@gmail.com>
2021-07-03 11:19:49 -0400

    modified testing stuffs.py to better search directories
    also added a logs text file. might come in handy

--- Dump stuff/testing stuffs.py
+++ Dump stuff/testing stuffs.py
@@ -1,18 +1,22 @@
 import os
 
-dir = os.getcwd()
+dir = os.getcwd() #gets and prints the current working directory
 print(dir)
-parent = os.path.dirname(dir)
+parent = os.path.dirname(dir) #gets the parent dir
 print(parent)
-dir = os.listdir(dir)
+dir = os.listdir(parent+"/graphics/maps") #looks in the graphics/maps folder
 
-files= []
+files= [] #json array
 
 for file in dir:
-    dot = file.rfind(".")
-    ext = file[dot:]
-    print(ext)
-    if ext == ".json":
-        files.append(file)
+    #looks for extensions and adds json extensions to json array. if its a folder, tell us
+    dot = file.rfind(".") 
+    if dot != -1:
+    	ext = file[dot:]
+    	print(ext)
+    	if ext == ".json":
+        	files.append(file)
+        	
+    else: print(file)
 
-print(files)
+print(files) #print the json array
