ae2052c338b6089d1ed92105f50157bc17ba69c4
MonEmperor <abr.da.king2016@gmail.com>
2022-02-22 01:57:10 -0500

    continued db research, also tried compiling the code

--- Dump stuff/db_testing/pokeapitesting.py
+++ Dump stuff/db_testing/pokeapitesting.py
@@ -1,3 +1,4 @@
+import time
 import requests
 import json
 from utils.json_utils import load_json, create_json, update_json
@@ -5,10 +6,9 @@ from utils.json_utils import load_json, create_json, update_json
 id = 1
 pokemon = f"https://pokeapi.co/api/v2/pokemon/{id}/"
 
-url = "https://pokeapi.co/api/v2/pokemon-species/1/"
-
+url = f"https://pokeapi.co/api/v2/growth-rate/{2}/"
 response = requests.request("GET", url=url)
-
 data = json.loads(response.text)
-update_json("dump.json", data)
-#print(json.dumps(data, indent=4, sort_keys=False))
+update_json("dump.json", data,operation="w")
+print(json.dumps(data, indent=4, sort_keys=False))
+#time.sleep(1)
