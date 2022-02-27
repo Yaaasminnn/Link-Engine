4d2c5dbd8563d5f7161b37a9c2762b4eb81f6e5b
lvlON-Emperor <46490084+MonEmperor@users.noreply.github.com>
2022-02-27 03:57:46 -0500

    worked more on db. halfway done?

--- Dump stuff/db_testing/pokeapitesting.py
+++ Dump stuff/db_testing/pokeapitesting.py
@@ -3,12 +3,22 @@ import requests
 import json
 from utils.json_utils import load_json, create_json, update_json
 
-id = 1
-pokemon = f"https://pokeapi.co/api/v2/pokemon/{id}/"
+id = 493
 
-url = f"https://pokeapi.co/api/v2/growth-rate/{2}/"
-response = requests.request("GET", url=url)
-data = json.loads(response.text)
-update_json("dump.json", data,operation="w")
-print(json.dumps(data, indent=4, sort_keys=False))
-#time.sleep(1)
+def call(print_resp=False, id=id):
+    url = f"https://pokeapi.co/api/v2/pokemon-form/{id}/"
+    response = requests.request("GET", url=url)
+    data = json.loads(response.text)
+    if print_resp:
+        print(json.dumps(data, indent=4, sort_keys=False))
+    else:
+        update_json("dump.json", data, operation="w")
+
+def list(print_resp=False):
+    begin, end = 1,5
+    for i in range(begin, end+1):
+        call(print_resp, id=i)
+        time.sleep(1)
+
+
+call()
