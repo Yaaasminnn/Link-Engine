23ff8246e30912d4643f39b20fa5882597b8babf
lvlON-Emperor <46490084+MonEmperor@users.noreply.github.com>
2022-03-17 20:52:42 -0400

    added a windows venv and batch script

--- game.bat
+++ game.bat
@@ -1,1 +1,2 @@
-
+@echo off
+.\venv_win\Scripts\python.exe .\main.py
--- utils/directories.py
+++ utils/directories.py
@@ -14,7 +14,9 @@ def get_project_dir():
 
     problem: may not be foolproof. need a surefire way to get the project dir no matter where the file is located
     """
-    os.system("pwd > project_dir.txt")
+    x=os.system("pwd > project_dir.txt") # echos the project directory to the project dir folder. assumes unix.
+    if x!=0: os.system("cd > project_dir.txt") # if not unix, it will be DOS/Windows
+
     with open("project_dir.txt", "r") as f:
         dir = f.read()
     os.chdir(dir[:-1])
